34e3176a18c3a01c1fe26a8ae35453b307888b65
Tiago Santos Palma <tiagosantos.mr@gmail.com>
2020-08-09 17:21:45 -0300

    home menu logo and buttons

--- Act_Menu_main.cs
+++ Act_Menu_main.cs
@@ -24,5 +24,12 @@ namespace FruitSales
 
 
         }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 }
