7ca128bc3be90a4fa38ee3b26038d5d87d7eeac4
Tiago Santos Palma <tiagosantos.mr@gmail.com>
2020-08-09 18:31:16 -0300

    changing colors, methods of the buttons created, scrowview

--- Act_Menu_main.cs
+++ Act_Menu_main.cs
@@ -12,17 +12,51 @@ using Android.Widget;
 
 namespace FruitSales
 {
-    [Activity(Label = "Act_Menu_main", Theme = "@style/AppTheme", MainLauncher = true, Icon = "@drawable/pineapple" )]
+    [Activity(Label = "Fruits", Theme = "@style/AppTheme", MainLauncher = true, Icon = "@drawable/pineapple" )]
     public class Act_Menu_main : Activity
     {
+        Button cmd_client;
+        Button cmd_products;
+        Button cmd_sales;
+        Button cmd_statistics;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            RequestWindowFeature(WindowFeatures.NoTitle);
             base.OnCreate(savedInstanceState);
 
             // Create your application here
             SetContentView(Resource.Layout.Layout_Menu_main);
 
+            cmd_client = FindViewById<Button>(Resource.Id.cmd_client);
+            cmd_products = FindViewById<Button>(Resource.Id.cmd_products);
+            cmd_sales = FindViewById<Button>(Resource.Id.cmd_sales);
+            cmd_statistics = FindViewById<Button>(Resource.Id.cmd_Statistics);
 
+            cmd_client.Click += Cmd_client_Click;       
+            cmd_products.Click += Cmd_products_Click;
+            cmd_sales.Click += Cmd_sales_Click;
+            cmd_statistics.Click += Cmd_statistics_Click;
+        }
+
+        private void Cmd_client_Click(object sender, EventArgs e)
+        {
+            //boxclient
+        }
+
+        private void Cmd_products_Click(object sender, EventArgs e)
+        {
+            //boxproducts 
+        }
+
+        private void Cmd_sales_Click(object sender, EventArgs e)
+        {
+            //boxsales
+        }
+
+        private void Cmd_statistics_Click(object sender, EventArgs e)
+        {
+            //boxstatistics
         }
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
