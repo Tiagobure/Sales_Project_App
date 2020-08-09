d578cbf503888e6c7a9bff0cd078f14e28a97ab3
Tiago Santos Palma <tiagosantos.mr@gmail.com>
2020-08-08 22:30:58 -0300

    Project app created

---
+++ Act_Menu_main.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace FruitSales
+{
+    [Activity(Label = "Act_Menu_main", Theme = "@style/AppTheme", MainLauncher = true, Icon = "@drawable/pineapple" )]
+    public class Act_Menu_main : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.Layout_Menu_main);
+
+
+        }
+    }
+}
